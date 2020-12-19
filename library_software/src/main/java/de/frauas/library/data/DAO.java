8f20705c537a43781612f97241b4d59a9508ee35
Leonard <leonard.husske@t-online.de>
2020-12-19 19:32:20 +0100

    Adding DAO Interface

---
+++ library_software/src/main/java/de/frauas/library/data/DAO.java
@@ -0,0 +1,20 @@
+package de.frauas.library.data;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO <T>{
+	
+	Optional<T> get(int id);
+	
+	List<T> getAll();
+	
+	void save(T t);
+	
+	void update(T t, String[] params);
+	
+	void delete(T t);
+	
+	void edit(T t, String param);
+
+}
