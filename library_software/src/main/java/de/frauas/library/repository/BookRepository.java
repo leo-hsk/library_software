47e2f97f7d8e728d1a1ffe7fdced32507e55bc02
Leonard <leonard.husske@t-online.de>
2020-12-19 21:09:52 +0100

    Adding Book Repository Interface

---
+++ library_software/src/main/java/de/frauas/library/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package de.frauas.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import de.frauas.library.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Integer>{
+
+}
