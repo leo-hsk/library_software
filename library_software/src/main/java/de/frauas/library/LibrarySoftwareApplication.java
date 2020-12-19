1d6c4c080a2cb00f10bd9b73a1500581b6db7970
Leonard <leonard.husske@t-online.de>
2020-12-19 19:21:46 +0100

    Commit all files

---
+++ library_software/src/main/java/de/frauas/library/LibrarySoftwareApplication.java
@@ -0,0 +1,13 @@
+package de.frauas.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySoftwareApplication.class, args);
+	}
+
+}
---
+++ library_software/src/test/java/de/frauas/library/LibrarySoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package de.frauas.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
