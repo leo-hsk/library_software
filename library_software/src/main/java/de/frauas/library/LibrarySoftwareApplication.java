7e387b5cdd8b4c5d9f702b481506af675084299c
Leonard <leonard.husske@t-online.de>
2020-12-19 21:10:57 +0100

    Deactivating Spring Security for development purposes

--- library_software/src/main/java/de/frauas/library/LibrarySoftwareApplication.java
+++ library_software/src/main/java/de/frauas/library/LibrarySoftwareApplication.java
@@ -2,8 +2,9 @@ package de.frauas.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) // Deactivating Security for development
 public class LibrarySoftwareApplication {
 
 	public static void main(String[] args) {
