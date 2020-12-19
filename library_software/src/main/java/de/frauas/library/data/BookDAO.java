0a7adc230ec3a2496ce9a8872974b28884829024
Leonard <leonard.husske@t-online.de>
2020-12-19 22:05:03 +0100

    Update neccesary methods

--- library_software/src/main/java/de/frauas/library/data/BookDAO.java
+++ library_software/src/main/java/de/frauas/library/data/BookDAO.java
@@ -1,6 +1,8 @@
 package de.frauas.library.data;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +12,12 @@ import de.frauas.library.model.Book;
 import de.frauas.library.repository.BookRepository;
 
 @Repository
-public class BookDAO implements DAO{
+public class BookDAO implements DAO<Book>{
+	
 	
 	@Autowired
 	BookRepository bookRepository;
-
-	@Override
-	public Optional get(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	
 	@Override
 	public List<Book> getAll() {
@@ -28,28 +25,49 @@ public class BookDAO implements DAO{
 		return bookList;
 	}
 
-	@Override
-	public void save(Object t) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
-	public void update(Object t, String[] params) {
-		// TODO Auto-generated method stub
-		
+	public Optional<Book> get(int id) {
+		try {
+			Optional<Book> book = bookRepository.findById(id);
+			return book;
+		} catch (Exception e) {
+			System.err.println("No Book with id " + id + " found!");
+			return Optional.empty();
+		}	
 	}
 
-	@Override
-	public void delete(Object t) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
-	public void edit(Object t, String param) {
-		// TODO Auto-generated method stub
-		
+	public void save(Book book) {
+		bookRepository.save(book);
 	}
+
+
+	@Override
+	public void update(Book book, String[] params) {
+		
+		book.setTitle(Objects.requireNonNull(params[0], "Title cannot be null!"));
+		book.setAuthors(Objects.requireNonNull(params[1], "Authors cannot be null!"));
+		book.setISBN13(Objects.requireNonNull(params[2], "ISBN cannot be null!"));
+		book.setPublicationDate(Date.valueOf(Objects.requireNonNull(params[3].toString(), "Publication date cannot be null!")));
+		book.setPublisher(Objects.requireNonNull(params[4], "Publisher cannot be null!"));
+		
+		bookRepository.save(book);
+	}
+
+
+	@Override
+	public void delete(Book book) {
+		bookRepository.delete(book);
+	}
+
+
+	@Override
+	public void edit(Book book, String param) {
+		book.setLent(Boolean.valueOf(param));
+		bookRepository.save(book);
+	}
+
 
 }
