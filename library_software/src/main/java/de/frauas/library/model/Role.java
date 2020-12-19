eb5b61144b023818fd040632bcaf125758b895b1
Leonard <leonard.husske@t-online.de>
2020-12-20 00:08:26 +0100

    Adding Role Entity - not finished yet

---
+++ library_software/src/main/java/de/frauas/library/model/Role.java
@@ -0,0 +1,38 @@
+package de.frauas.library.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	
+	@Id
+	@Column(name = "role_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	
+	// Need to add relationship to User
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
