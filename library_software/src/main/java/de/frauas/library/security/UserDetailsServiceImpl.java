b66ed304ada1f2770d307027abb6d86aaf776676
Leonard <leonard.husske@t-online.de>
2020-12-20 19:29:23 +0100

    Adding UserServiceImplementation

---
+++ library_software/src/main/java/de/frauas/library/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package de.frauas.library.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import de.frauas.library.model.User;
+import de.frauas.library.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository UserRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = UserRepository.getUserByUsername(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("Could not find User '" + username + "'.");
+		}
+		
+		return new MyUserDetails(user);
+	}
+
+}
