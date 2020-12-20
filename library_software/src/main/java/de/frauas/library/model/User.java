0556dcfc77d7241b1814a4b1bf242a87497606e0
Leonard <leonard.husske@t-online.de>
2020-12-20 19:23:51 +0100

    Updating User

--- library_software/src/main/java/de/frauas/library/model/User.java
+++ library_software/src/main/java/de/frauas/library/model/User.java
@@ -5,7 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users")
@@ -15,66 +18,94 @@ public class User {
 	@Column(name = "user_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String username;
-	private String password;
-	private String enabled;
 	
+	@NotNull
+	private String username;
+	
+	@NotNull
+	private String password;
+	
+	@Column(columnDefinition = "boolean default true")
+	private boolean enabled;
+	
+	@NotNull
 	private String firstName;
+	
+	@NotNull
 	private String lastName;
+	
+	@NotNull
 	private String email;
 	
- // Need to add relationship to role
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "role_id")
+	private Role role;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getEnabled() {
+
+	public boolean isEnabled() {
 		return enabled;
 	}
-	public void setEnabled(String enabled) {
+
+	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Role getRole() {
 		return role;
 	}
+
 	public void setRole(Role role) {
 		this.role = role;
 	}
 	
-	
-
 	
 	
 }
