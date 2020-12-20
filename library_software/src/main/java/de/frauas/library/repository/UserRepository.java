d9d9fba523152084a5c102dbb63050dfab3b49e0
Leonard <leonard.husske@t-online.de>
2020-12-20 19:25:46 +0100

    Adding UserRepository

---
+++ library_software/src/main/java/de/frauas/library/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package de.frauas.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import de.frauas.library.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	@Query("SELECT u from User u WHERE u.username = :username")
+	public User getUserByUsername(@Param("username") String username);
+	
+}
