0836c7b138f8e0a2608d93fa88d27faf9137ff64
Leonard <leonard.husske@t-online.de>
2020-12-23 09:10:27 +0100

    Chaning Id to long

--- library_software/src/main/java/de/frauas/library/repository/UserRepository.java
+++ library_software/src/main/java/de/frauas/library/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import de.frauas.library.model.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query("SELECT u from User u WHERE u.username = :username")
 	public User getUserByUsername(@Param("username") String username);
