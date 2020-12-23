ee882829fc94497740bac206dca78954fd113df9
Leonard <leonard.husske@t-online.de>
2020-12-23 19:24:23 +0100

    Deleting imports which are not needed

--- library_software/src/main/java/de/frauas/library/repository/BookDataRepository.java
+++ library_software/src/main/java/de/frauas/library/repository/BookDataRepository.java
@@ -1,9 +1,6 @@
 package de.frauas.library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import de.frauas.library.model.BookData;
 
 public interface BookDataRepository extends JpaRepository<BookData, Long>{
