94acc9af0bfc9da6081210d260bb264ea2bd90dd
Leonard <leonard.husske@t-online.de>
2020-12-23 23:11:44 +0100

    Updating addBookData function, so that it works with form

--- library_software/src/main/java/de/frauas/library/controller/BookDataController.java
+++ library_software/src/main/java/de/frauas/library/controller/BookDataController.java
@@ -1,26 +1,31 @@
 package de.frauas.library.controller;
 
+import java.sql.Date;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponents;
+
 import org.springframework.web.util.UriComponentsBuilder;
 
 import de.frauas.library.data.BookDataDAO;
+import de.frauas.library.form.BookForm;
 import de.frauas.library.model.BookData;
 
-@RestController
+@Controller
 public class BookDataController {
 	
 	@Autowired
@@ -37,18 +42,36 @@ public class BookDataController {
 		}
 	}
 	
-	@PostMapping(value = "/bookData", consumes = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public ResponseEntity<Object> addBookData(@RequestBody BookData bookData, UriComponentsBuilder builder) {
+	@PostMapping(value = "/addBook")
+	public String addBookData(Model model, @ModelAttribute("bookForm") BookForm bookForm) {
+
+		String title = bookForm.getTitle();
+		String authors = bookForm.getAuthors();
+		String publisher = bookForm.getPublisher();
+		Date publicationDate = bookForm.getPublicationDate();
+		long isbn13 = bookForm.getIsbn13();
+		
+		if (title != null
+				&& title.length() > 0
+				&& authors != null
+				&& authors.length() > 0
+				&& publisher != null
+				&& publisher.length() > 0
+				&& publicationDate != null
+				&& !String.valueOf(isbn13).isBlank()
+				&& String.valueOf(isbn13).length() == 13) {
+		
+		BookData bookData = new BookData(title, authors, publisher, publicationDate, isbn13);
+		
 		bookDataDAO.save(bookData);
-		UriComponents path = builder.path("/bookData").path(String.valueOf(bookData.getIsbn13())).build();
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Location", path.toUriString());
-		return new ResponseEntity<Object>(headers, HttpStatus.CREATED);
+		return "addBook";
+		}
+		
+		model.addAttribute("errorMessage", "Submission is not valid.");
+		return "addBook";
 	}
 	
 	@DeleteMapping(value = "/bookData/{isbn13}")
-	@ResponseBody
 	public ResponseEntity<Object> deleteBookData(@PathVariable("isbn13") long isbn13, UriComponentsBuilder builder) {	
 		if(bookDataDAO.get(isbn13).isEmpty()) {
 			return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
