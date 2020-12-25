9e7f08f953906a85de05d32198d7f92edd21d88d
Leonard <leonard.husske@t-online.de>
2020-12-25 14:20:40 +0100

    Adding search query

--- library_software/src/main/java/de/frauas/library/repository/BookRepository.java
+++ library_software/src/main/java/de/frauas/library/repository/BookRepository.java
@@ -1,9 +1,18 @@
 package de.frauas.library.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import de.frauas.library.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
+	
+	@Query(value = "SELECT * FROM books b LEFT JOIN book_data bd "
+			+ "ON b.isbn13 = bd.isbn13 "
+			+ "WHERE b.isbn13 = ?1 "
+			+ "OR MATCH(bd.title, bd.authors, bd.publisher) AGAINST(?1)", nativeQuery = true)
+	public List<Book> search(String keyword);
 
 }
