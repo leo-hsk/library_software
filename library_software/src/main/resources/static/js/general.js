575a648f3f5e55007a860b5ddb8cd2cf4f363ab1
Leonard <leonard.husske@t-online.de>
2020-12-28 15:36:26 +0100

    Delete Book Validation

--- library_software/src/main/resources/static/js/general.js
+++ library_software/src/main/resources/static/js/general.js
@@ -4,14 +4,23 @@ function deleteUserConfirm() {
 	return confirm('Do you really want to delete your Account?');
 }
 
-function deleteBookConfirm() {
-	return confirm('Do you really want to delete the book?');
-}
-
 function deleteUserConfirmAdmin() {
 	return confirm('Do you really want to delete this user?');
 }
 
+function deleteBookConfirm(id) {
+	var num = id.replace(/\D/g, "");
+	var id_lend = "lend" + num;
+	var element = document.getElementById(id_lend);
+	if (element.innerHTML === "No") {
+		return confirm('Do you really want to delete the book?');
+	} else {
+		alert("Can not delete book, because book is not returned yet.");
+		return false;
+	}
+
+	
+}
 
 // Copy password to hidden field
 function copyPassword() {
