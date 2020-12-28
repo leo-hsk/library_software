38628f815501dfcd4af4a05c02488494a8144710
Leonard <leonard.husske@t-online.de>
2020-12-28 16:27:38 +0100

    Adding validation js

--- library_software/src/main/resources/static/js/general.js
+++ library_software/src/main/resources/static/js/general.js
@@ -43,3 +43,13 @@ var regEx = /^\d{4}-\d{2}-\d{2}$/;
    return false;
  }
 }
+
+function validateEmail() {
+	var email = document.forms["registerUser"]["email"].value;
+	if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
