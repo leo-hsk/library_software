880d61affc53956e4f1416dcfcf30a9759312383
Leonard <leonard.husske@t-online.de>
2020-12-29 19:31:15 +0100

    Deleting auto security configuration

--- library_software/src/main/java/de/frauas/library/LibrarySoftwareApplication.java
+++ library_software/src/main/java/de/frauas/library/LibrarySoftwareApplication.java
@@ -2,9 +2,8 @@ package de.frauas.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) // Deactivating Security for development
+@SpringBootApplication()
 public class LibrarySoftwareApplication {
 
 	public static void main(String[] args) {
