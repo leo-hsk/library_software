5db768218cf308739aef08e6391569da162e179b
Leonard <leonard.husske@t-online.de>
2020-12-29 20:57:32 +0100

    JS Formatting

--- library_software/src/main/resources/static/js/general.js
+++ library_software/src/main/resources/static/js/general.js
@@ -18,38 +18,35 @@ function deleteBookConfirm(id) {
 		alert("Can not delete book, because book is not returned yet.");
 		return false;
 	}
-
-	
 }
 
 // Copy password to hidden field
 function copyPassword() {
-var box1 = document.getElementById('password');
-var box2 = document.getElementById('pw');
-box2.value = box1.value;
+	var box1 = document.getElementById('password');
+	var box2 = document.getElementById('pw');
+	box2.value = box1.value;
 };
 
 function validateBookAddForm() {
-var isbn = document.forms["bookAddForm"]["isbn13"].value;
-  if (isbn.length !== 13) {
-   alert("Given ISBN 13 is not valid or empty.");
-   return false;
- }
+	var isbn = document.forms["bookAddForm"]["isbn13"].value;
+	if (isbn.length !== 13) {
+		alert("Given ISBN 13 is not valid or empty.");
+		return false;
+	};
 
-var dateString = document.forms["bookAddForm"]["publicationDate"].value;
-var regEx = /^\d{4}-\d{2}-\d{2}$/;
-  if(!dateString.match(regEx)) {
-   alert("Date needs to be in format 'YYYY-MM-DD'.");
-   return false;
- }
-}
+	var dateString = document.forms["bookAddForm"]["publicationDate"].value;
+	var regEx = /^\d{4}-\d{2}-\d{2}$/;
+	if(!dateString.match(regEx)) {
+		alert("Date needs to be in format 'YYYY-MM-DD'.");
+		return false;
+	}
+};
 
 function validateEmail() {
 	var email = document.forms["registerUser"]["email"].value;
-	if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
-    return (false)
-}
+	if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
+		return true;
+	}
+	alert("You have entered an invalid email address!")
+	return false;
+};
